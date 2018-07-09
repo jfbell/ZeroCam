6553b2d7241190d88f03818819c73a4b154c41ac
James Bell <james@belldesign.com.au>
2018-07-09 07:21:41 -0700

    adding resin-cpp-hello-world as reference of something that worked

--- serverRx/serverRx.py
+++ serverRx/serverRx.py
@@ -1,28 +1,46 @@
 import socket
 import subprocess
+import os
 
-# Start a socket listening for connections on 0.0.0.0:8000 (0.0.0.0 means
-# all interfaces)
-server_socket = socket.socket()
-server_socket.bind(('0.0.0.0', 5777))
-server_socket.listen(0)
-
-# Accept a single connection and make a file-like object out of it
-connection = server_socket.accept()[0].makefile('rb')
 try:
+    # Start a socket listening for connections on 0.0.0.0:8000 (0.0.0.0 means
+    # all interfaces)
+    path = "/tmp/buf.fifo"
+    if os._exists(path):
+        os.rmdir(path)
+    os.mkfifo(path)
+    server_socket = socket.socket()
+    server_socket.bind(('0.0.0.0', 5777))
+    server_socket.listen(0)
+    print('server up')
+
+    # Accept a single connection and make a file-like object out of it
+    connection = server_socket.accept()[0].makefile('rb')
+    print('connection made')
     # Run a viewer with an appropriate command line. Uncomment the mplayer
     # version if you would prefer to use mplayer instead of VLC
-    cmdline = ['vlc', '--demux', 'h264', '-']
+    # cmdline = ['vlc', '--demux', 'h264', '-']
+    # print('vlc called')
+
+
     #cmdline = ['mplayer', '-fps', '25', '-cache', '1024', '-']
-    player = subprocess.Popen(cmdline, stdin=subprocess.PIPE)
+    # player = subprocess.Popen(cmdline, stdin=subprocess.PIPE)
+    # print('subprocess called \n entering while')
+    fifo = open(path, "wb")
+    print('fifo open')
+
     while True:
         # Repeatedly read 1k of data from the connection and write it to
         # the media player's stdin
         data = connection.read(1024)
         if not data:
             break
-        player.stdin.write(data)
+        fifo.write(data)
+        # player.stdin.write(data)
 finally:
     connection.close()
     server_socket.close()
-    player.terminate()
+    # player.terminate()
+    fifo.close()
+    os.rmdir(path)
+    print('all closed')
